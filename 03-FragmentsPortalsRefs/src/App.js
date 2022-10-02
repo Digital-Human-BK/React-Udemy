5127744911851a88a6bfe0620925c12a0ea5c4f5
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-02 20:58:48 +0300

    Learn about Fragments, Portals and Refs

---
+++ 03-FragmentsPortalsRefs/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <>
+      <React.Fragment>
+        <AddUser onAddUser={addUserHandler} />
+        {usersList.length > 0 && <UsersList users={usersList} />}
+      </React.Fragment>
+    </>
+  );
+}
+
+export default App;
