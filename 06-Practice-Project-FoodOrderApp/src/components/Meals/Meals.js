5d37d9c57f0911eee6e83568b003b3c71ce64aab
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-06 15:50:58 +0300

    Added Meals list and its children components

---
+++ 06-Practice-Project-FoodOrderApp/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import AvailableMeals from './AvailableMeals';
+// import classes from './Meals.module.css';
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
