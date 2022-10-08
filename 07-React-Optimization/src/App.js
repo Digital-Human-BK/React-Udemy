1c83e16821d00c79b6e76e4bde60e5dc72e71bc8
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-08 16:00:08 +0300

    Learn 'useCallback'

--- 07-React-Optimization/src/App.js
+++ 07-React-Optimization/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -6,15 +6,29 @@ import Button from './components/UI/Button/Button';
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowToggleP, setAllowToggleP] = useState(false);
+
   console.log('APP RUNNING');
-  const toggleP = () => {
-    setShowP(prev => !prev);
-  }
+
+  const toggleP = useCallback(() => {
+    if (allowToggleP) {
+      setShowP((prev) => !prev);
+    }
+  }, [allowToggleP]);
+
+  const allowToggle = useCallback(() => {
+    setAllowToggleP((prev) => !prev);
+    console.log('Allow Toggle');
+  }, []);
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
+      <Button onClick={allowToggle}>Allow Toggle</Button>
+      <br />
+      <br />
       <Button onClick={toggleP}>Show Paragraph</Button>
+      <DemoOutput show={showP} />
     </div>
   );
 }
