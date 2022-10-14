b8cc49aa9f27fbf4d8f9928f280dbb73bcb4d100
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-14 16:30:22 +0300

    Use cutom hook for form validation

--- 12-Forms-And-User-Input/src/hooks/use-input.js
+++ 12-Forms-And-User-Input/src/hooks/use-input.js
@@ -1,32 +1,58 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
+const initialState = {
+  value: '',
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT') {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === 'BLUR') {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === 'RESET') {
+    return { value: '', isTouched: false };
+  }
+  return initialState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [state, dispatchState] = useReducer(inputStateReducer, initialState);
+  // const [enteredValue, setEnteredValue] = useState('');
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const isValid = validateValue(enteredValue);
-  const hasError = !isValid && isTouched;
+  // const isValid = validateValue(enteredValue);
+  // const hasError = !isValid && isTouched;
+
+  const isValid = validateValue(state.value);
+  const hasError = !isValid && state.isTouched;
 
   const valueChangeHandler = (ev) => {
-    setEnteredValue(ev.target.value);
+    dispatchState({ type: 'INPUT', value: ev.target.value });
+    // setEnteredValue(ev.target.value);
   };
 
   const inputBlurHandler = () => {
-    setIsTouched(true);
+    dispatchState({ type: 'BLUR' });
+    // setIsTouched(true);
   };
 
   const reset = () => {
-    setEnteredValue('');
-    setIsTouched(false);
-  }
+    dispatchState({ type: 'RESET' });
+    // setEnteredValue('');
+    // setIsTouched(false);
+  };
 
   return {
-    value: enteredValue,
+    // value: enteredValue,
+    value: state.value,
     isValid,
     hasError,
     valueChangeHandler,
     inputBlurHandler,
-    reset
+    reset,
   };
 };
 
@@ -54,7 +80,7 @@ export default useInput;
 //   return {
 //     enteredValue,
 //     isValid,
-//     hasError, 
+//     hasError,
 //     valueChangeHandler,
 //     inputBlurHandler,
 //     reset
