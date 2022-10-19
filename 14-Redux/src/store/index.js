c035b888605d3389f19d8346770a34cda0835e88
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-19 15:07:32 +0300

    Introducing Redux Toolkit

--- 14-Redux/src/store/index.js
+++ 14-Redux/src/store/index.js
@@ -1,6 +1,29 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter++;
+    },
+    increase(state, action) {
+      state.counter += action.value;
+    },
+    decrease(state, action) {
+      state.counter -= action.value;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'INC') {
@@ -25,11 +48,11 @@ const counterReducer = (state = initialState, action) => {
     };
   }
 
-  if(action.type ==='TOGGLE'){
+  if (action.type === 'TOGGLE') {
     return {
       counter: state.counter,
-      showCounter: !state.showCounter
-    }
+      showCounter: !state.showCounter,
+    };
   }
 
   return state;
