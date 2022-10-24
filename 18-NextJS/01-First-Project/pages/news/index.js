747ba6657a84a7726ac6bc56309088cdadb2e980
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-24 13:51:27 +0300

    Creating navigation using <Link>

--- 18-NextJS/01-First-Project/pages/news/index.js
+++ 18-NextJS/01-First-Project/pages/news/index.js
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>News Page</h1>
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/great-framework'>NextJS is great framework!</Link>
+        </li>
+        <li>
+          <Link href='/news/basics'>Discover the basics</Link>
+        </li>
+        <li>
+          <Link href='/news/advanced'>Advanced stuff</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
