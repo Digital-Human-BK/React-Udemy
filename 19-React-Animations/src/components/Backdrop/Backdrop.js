ec015132471d33677516378eb1d550ddd9684232
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-10-25 12:42:20 +0300

    React Animations

---
+++ 19-React-Animations/src/components/Backdrop/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Backdrop.css';
+
+const backdrop = (props) => {
+  const cssClasses = `Backdrop ${props.show ? 'BackdropOpen': 'BackdropClosed'}`
+  return <div className={cssClasses}></div>;
+};
+
+export default backdrop;
---
+++ 19-React-Animations/src/components/Modal/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Modal.css';
+
+const modal = (props) => {
+  const cssClasses = `Modal ${props.show ? 'ModalOpen' : 'ModalClosed'}`;
+
+  return (
+    <div className={cssClasses}>
+      <h1>A Modal</h1>
+      <button className='Button' onClick={props.closed}>
+        Dismiss
+      </button>
+    </div>
+  );
+};
+
+export default modal;
